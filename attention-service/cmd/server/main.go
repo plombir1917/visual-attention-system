09b66e210e613abc995d01578a2cba9300b35a44
plombir1917 <dfhhggh@list.ru>
2026-03-12 13:40:57 +0300

    feat(main.go): server start init

--- attention-service/cmd/server/main.go
+++ attention-service/cmd/server/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"attention-service/internal/api"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	router := api.NewRouter()
+
+	fmt.Println("Server started on 8080")
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		fmt.Println("ListenAndServe:", err)
+	}
+
+}
