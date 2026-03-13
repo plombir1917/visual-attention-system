1947a06ad909b536221134cfae4c3f8c59f9d1d3
plombir1917 <dfhhggh@list.ru>
2026-03-13 16:13:43 +0300

    feat(atttention-service): first redis connection

--- attention-service/internal/service/attention_service.go
+++ attention-service/internal/service/attention_service.go
@@ -1,12 +1,32 @@
 package service
 
 import (
+	"attention-service/storage"
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+var ctx = context.Background()
+
 func ProcessMessage(clientWS *websocket.Conn, modelWS *websocket.Conn) {
+	cfg := storage.Config{
+		Addr:        "",
+		Password:    "",
+		User:        "",
+		DB:          11,
+		MaxRetries:  10,
+		DialTimeout: time.Second * 10,
+		Timeout:     time.Second * 10,
+	}
+
+	db, err := storage.NewClient(ctx, cfg)
+	if err != nil {
+		panic(err)
+	}
+
 	for {
 		// читаем сообщение
 		_, msg, err := clientWS.ReadMessage()
@@ -28,6 +48,12 @@ func ProcessMessage(clientWS *websocket.Conn, modelWS *websocket.Conn) {
 			fmt.Println("Model read:", err)
 			break
 		}
+
+		// REDIS HERE
+		if err := db.Set(ctx, time.Now().String(), msg, 2*time.Hour).Err(); err != nil {
+			fmt.Printf("failed to set data, error: %s", err.Error())
+		}
+		//
 
 		// отправляем клиенту полученное сообщение из модели
 		if err = clientWS.WriteMessage(websocket.TextMessage, msg); err != nil {
